1d2bd5abe0bb63b472e2d08e3c9b81ca1ff5d51f
Christian Spies <christian.spies@automit.de>
2024-09-23 22:26:37 +0200

    fix: ad auth

--- Program.cs
+++ Program.cs
@@ -2,13 +2,14 @@
 using DotNetCoreSqlDb.Data;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpContextAccessor();
+
 // Add database context and cache
 if(builder.Environment.IsDevelopment())
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
     builder.Services.AddDistributedMemoryCache();
-    builder.Services.AddHttpContextAccessor();
     {
 }
 }
@@ -18,7 +19,6 @@ else
         options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
     builder.Services.AddStackExchangeRedisCache(options =>
     {
-    builder.Services.AddHttpContextAccessor();
     options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
     options.InstanceName = "SampleInstance";
     });
